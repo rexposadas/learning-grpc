ed9efe6abc3803318a2ae7958945841780dc03f8
Rex Posadas <rposadas@redwoodlogistics.com>
2022-07-16 20:36:06 +0800

    initial files, can start server and client

---
+++ greet/client/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("failed to connect %v", err)
+	}
+	defer conn.Close()
+
+}
---
+++ greet/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/rexposadas/learning-grpc/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen on  %v", err)
+	}
+
+	log.Println("listening on ", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Printf("failed to serve %v", err)
+	}
+}
